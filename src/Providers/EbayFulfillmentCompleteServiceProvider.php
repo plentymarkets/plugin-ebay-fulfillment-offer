d2eb3bf776c46124e1f69f5eb0ae2259f6600ae4
Ihab Hussein <ihab.hussein@plentymarkets.com>
2019-08-27 14:02:35 +0200

    - rename plugin

---
+++ src/Providers/EbayFulfillmentCompleteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php // strict
+
+namespace EbayFulfillmentComplete\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class EbayFulfillmentCompleteServiceProvider
+ *
+ * @package EbayFulfillmentComplete\Providers
+ */
+class EbayFulfillmentCompleteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the core functions
+     */
+    public function register(){}
+}
